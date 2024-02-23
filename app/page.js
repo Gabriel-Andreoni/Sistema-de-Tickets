76a7fe5766681e03556e487ee589bac905dba859
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-02-23 20:22:34 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
   });
 
   const onSubmit = async (data) => {
-    await fetch(`${VERCEL_URL}/tickets`, {
+    await fetch(`${VERCEL_URL}`, {
       method: 'POST',
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify({
