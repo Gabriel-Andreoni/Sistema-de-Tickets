4d8e33795b2c076c0398c8b57dd3c075dcc0de08
Gabriel Andreoni <gabriel.andreoni@hotmail.com>
2024-03-02 17:58:14 -0300

    Update route.js

--- app/tickets/[id]/route.js
+++ app/tickets/[id]/route.js
@@ -1,7 +1,18 @@
 export async function GET(request, params) {
+    const slug = params.slug;
+   return new Response(JSON.stringify({
+    id: slug,
+}))
+}
+
+export async function POST(request, params) {
+    const body = await request.json();
    
+    return new Response(JSON.stringify({
+        data: body
+    }))
 }
 
 export async function DELETE(request, params) {
-    // Seu código de exclusão aqui
+   
 }
